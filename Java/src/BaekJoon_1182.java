b2235fcc3246188ada8901b27b920a7d45f0ce4a
hyuny <notimeforlunchs@gmail.com>
2020-05-16 23:09:33 +0900

    - BackJoon 1182

---
+++ Java/src/BaekJoon_1182.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// https://www.acmicpc.net/problem/1182
+public class BaekJoon_1182 {
+
+    public static int count = 0;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] temp = br.readLine().split(" ");
+        int n = Integer.parseInt(temp[0]);
+        int s = Integer.parseInt(temp[1]);
+
+        int[] nums = new int[n];
+        temp = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(temp[i]);
+        }
+
+        count(nums, 0, 0, s);
+
+        if(s == 0){
+            count--;
+        }
+        System.out.print(count);
+
+    }
+
+    public static void count(int[] nums, int currentIndex, int sum, int s) {
+
+        if (currentIndex == nums.length) {
+            if (sum == s) {
+                count++;
+            }
+            return;
+        }
+
+        count(nums, currentIndex + 1, sum + nums[currentIndex], s);
+        count(nums, currentIndex + 1, sum, s);
+    }
+}
