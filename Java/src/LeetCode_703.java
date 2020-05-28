b20f84235d771b90b7c9792cad9f7dc90fa8f6ac
reallucky84 <reallucky84@gmail.com>
2020-05-28 16:29:15 +0900

    - LeetCode 703

---
+++ Java/src/LeetCode_703.java
@@ -0,0 +1,28 @@
+import java.util.PriorityQueue;
+
+// https://leetcode.com/problems/kth-largest-element-in-a-stream/
+public class LeetCode_703 {
+
+    class KthLargest {
+
+        private int k;
+        private PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        public KthLargest(int k, int[] nums) {
+            this.k = k;
+            for(int n: nums){
+                priorityQueue.offer(n);
+            }
+        }
+
+        public int add(int val) {
+            priorityQueue.offer(val);
+            while(k < priorityQueue.size()){
+                priorityQueue.poll();
+            }
+            return priorityQueue.peek();
+        }
+    }
+}
+
+
