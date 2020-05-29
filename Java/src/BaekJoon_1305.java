897186addadaffb9d4856638b19f6aad28faa249
reallucky84 <reallucky84@gmail.com>
2020-05-29 16:45:05 +0900

    - BaekJoon 1305

--- Java/src/BaekJoon_1305.java
+++ Java/src/BaekJoon_1305.java
@@ -4,28 +4,23 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.List;
 
-// https://www.acmicpc.net/problem/2902
-public class BaekJoon_1796 {
+// https://www.acmicpc.net/problem/1305
+public class BaekJoon_1305 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String t = br.readLine();
-        String p = br.readLine();
+        int n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
 
-        int[] f = makeFail(p);
-        List<Integer> list = kmp(t, p, f);
-        System.out.println(list.size());
-        for (Integer i : list) {
-            System.out.println(i + 1);
-        }
+        System.out.println(n - makeFail(s)[n - 1]);
+
     }
 
     public static List<Integer> kmp(String s, String p, int[] f) {
+        List<Integer> list = new LinkedList<>();
         int lenS = s.length();
         int lenP = p.length();
         int j = 0;
-
-        List<Integer> list = new LinkedList<>();
         for (int i = 0; i < lenS; i++) {
             while (j > 0 && s.charAt(i) != p.charAt(j)) j = f[j - 1];
             if (s.charAt(i) == p.charAt(j)) {
@@ -38,13 +33,13 @@ public class BaekJoon_1796 {
             }
         }
         return list;
+
     }
 
     public static int[] makeFail(String p) {
         int len = p.length();
         int[] f = new int[len];
         f[0] = 0;
-
         int j = 0;
         for (int i = 1; i < len; i++) {
             while (j > 0 && p.charAt(i) != p.charAt(j)) j = f[j - 1];
@@ -56,4 +51,6 @@ public class BaekJoon_1796 {
         }
         return f;
     }
+
+
 }
