9c70184077a505bc630e9328d31a5d8f3b9b7830
reallucky84 <reallucky84@gmail.com>
2020-06-03 00:38:29 +0900

    - BaekJoon 1796 링크 수정

--- Java/src/BaekJoon_1796.java
+++ Java/src/BaekJoon_1796.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.List;
 
-// https://www.acmicpc.net/problem/2902
+// https://www.acmicpc.net/problem/1796
 public class BaekJoon_1796 {
 
     public static void main(String[] args) throws IOException {
